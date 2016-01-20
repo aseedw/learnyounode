4a9b7060bb525c5f97d328e7a99a1900747ce876
jeffcheng <b98502008@gmail.com>
2016-01-20 14:23:03 +0800

    Create http_collect.js

---
+++ http_collect.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var info = [];
+var url = process.argv[2];
+http.get(url, function callback (response) { 
+  response.setEncoding("utf8");
+
+  response.on("data", function (data) { 
+    info.push(data);
+  })
+  response.on("error",console.error);
+  response.on("end",function(){
+    console.log(info.join("").length);
+    console.log(info.join(""));
+  });
+});
