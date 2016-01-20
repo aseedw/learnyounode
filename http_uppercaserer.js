a47e50bc464254f2a2012b6b0833c4b3ef31c441
jeffcheng <b98502008@gmail.com>
2016-01-20 14:26:46 +0800

    Create http_uppercaserer.js

---
+++ http_uppercaserer.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    if(req.method != "POST") res.end('I respond to POST only');
+
+    var body = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk){
+        body+=chunk;
+    })
+
+    req.on('end', function(){
+        res.end(body.toUpperCase());
+    })
+
+});
+server.listen(process.argv[2]);
