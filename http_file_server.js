c71ecc3a527634ea41c061755c16f17a8bd14ed2
jeffcheng <b98502008@gmail.com>
2016-01-20 14:26:23 +0800

    Update http_file_server.js

--- http_file_server.js
+++ http_file_server.js
@@ -10,23 +10,5 @@ var server = http.createServer(
 );
 
 server.listen(process.argv[2]);
-*/
 
-/* learnyounode HTTP UPPERCASERER
-var http = require('http');
 
-var server = http.createServer(function(req, res) {
-    if(req.method != "POST") res.end('I respond to POST only');
-
-    var body = '';
-    req.setEncoding('utf8');
-    req.on('data', function(chunk){
-        body+=chunk;
-    })
-
-    req.on('end', function(){
-        res.end(body.toUpperCase());
-    })
-
-});
-server.listen(process.argv[2]);
